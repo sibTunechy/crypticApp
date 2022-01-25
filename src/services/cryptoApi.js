44594b412768c982b44fb04495b53fdb20bbbcfe
sibTunechy <ayotemitope930@gmail.com>
2022-01-25 07:47:50 +0100

    first upload

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,26 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '4e12f693e6mshba9ad3a5a1ccacep15f756jsn793091c0f426'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({url, headers: cryptoApiHeaders});
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl}),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+})
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
+
+
